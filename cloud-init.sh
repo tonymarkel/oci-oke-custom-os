75502573f894ecefae70f7909e967139abe91475
Tony Markel <tony.markel@oracle.com>
2026-03-11 10:56:05 -0400

    adding bash scripts to the repo

---
+++ cloud-init.sh
@@ -0,0 +1,51 @@
+    #!/bin/bash
+set -e
+set -o pipefail
+
+kubernetes_version="1.34.0"
+
+### DO NOT EDIT BELOW THIS LINE ###
+
+oke_repo_version="${kubernetes_version%.*}"
+oci_region=$(curl -H "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/ | grep "canonicalRegionName" | sed -E 's/.*"([^"]+)".*/\1/')
+
+function yum_repo_setup() {
+  local repo=$(cat <<'EOL'
+[oke-packages]
+baseurl = https://odx-oke.objectstorage.canonicalRegionName.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/yum/kubernetes/oke_repo_version/os/$releasever/$basearch
+gpgcheck = 0
+repo_gpgcheck = 0
+name = 'Oracle $releasever Container Engine for Kubernetes Nodes ($basearch)'
+EOL
+)
+  echo "$repo" > /etc/yum.repos.d/oke-packages.repo
+  sed -i "s/oke_repo_version/$oke_repo_version/g" /etc/yum.repos.d/oke-packages.repo
+  sed -i "s/canonicalRegionName/$oci_region/g" /etc/yum.repos.d/oke-packages.repo
+  echo "$(date) Yum repos: $(ls -m /etc/yum.repos.d)"
+  yum repolist
+}
+
+mkdir -p /etc/rpm
+RPM_SPEC=/etc/rpm/macros.verify
+### add override to OSMS to enable oke_node repo
+echo "Checking if OSMS plugin is enabled in this instance.."
+OSMS_PLUGIN_CONF_PATH="/etc/yum/pluginconf.d/osmsplugin.conf"
+OSMS_ENABLED=0
+if [ -e "$OSMS_PLUGIN_CONF_PATH" ]; then
+  OSMS_ENABLED=$(awk -F ' *= *' -v k="enabled" '/^[^#]/ && $1 == k {print $2}' /etc/yum/pluginconf.d/osmsplugin.conf)
+fi
+echo "$(date) Creating OSMS override for oke-packages repo.."
+mkdir -p /etc/oracle-cloud-agent/plugins/osms/
+echo "oke-packages.repo" >> /etc/oracle-cloud-agent/plugins/osms/ignored_repos.conf
+if [ "$OSMS_ENABLED" -eq 1 ]; then
+  echo "$(date) OSMS plugin is enabled in this instance, restart oracle-cloud-agent.service"
+  systemctl restart oracle-cloud-agent.service
+fi
+# disable rpm digest check to allow package installation without sha256 digest
+echo "%_pkgverify_level none" > $RPM_SPEC
+
+mkdir -p /etc/yum.repos.d
+yum_repo_setup
+yum install -y oci-oke-node-all-$kubernetes_version
+
+rm -f $RPM_SPEC
---
+++ oke-install.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+set -xe
+set -o pipefail
+
+function has_api_server() {
+  if [[ -n "$(curl --fail --silent --retry-delay 1 --retry 5 -H "Authorization: Bearer Oracle" -L0 http://169.254.169.254/opc/v2/instance/metadata/ansible_args | base64 -d | jq .k8s_apiserver_addr)" ]]; then
+    return 0
+  elif [ -n "$(curl --fail --silent --retry-delay 1 --retry 5 -H "Authorization: Bearer Oracle" -L0 http://[fd00:c1::a9fe:a9fe]/opc/v2/instance/metadata/ansible_args | base64 -d | jq .k8s_apiserver_addr)" ]; then
+    return 0
+  fi
+  return 1
+}
+
+function restart_crio() {
+  image_name=$(curl --fail --silent --retry-delay 1 --retry 5 -H "Authorization: Bearer Oracle" -L0 http://169.254.169.254/opc/v2/instance/metadata/oke-image-name)
+  if [[ -z "$image_name" ]]; then
+    image_name=$(curl --fail --silent --retry-delay 1 --retry 5 -H "Authorization: Bearer Oracle" -L0 http://[fd00:c1::a9fe:a9fe]/opc/v2/instance/metadata/oke-image-name)
+  fi
+  if [[ -n "$image_name" ]] && [[ "$image_name" == "Oracle-Linux-7.9"* ]] && [[ "$image_name" == *"OKE-1.30.10"* ]]; then
+    systemctl restart crio
+  fi
+}
+
+# Allow user to specify arguments through custom cloud-init
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case "$key" in
+    --kubelet-extra-args)
+      export KUBELET_EXTRA_ARGS="$2"
+      shift
+      shift
+      ;;
+    --cluster-dns)
+      export CLUSTER_DNS="$2"
+      shift
+      shift
+      ;;
+    --apiserver-endpoint)
+      export APISERVER_ENDPOINT="$2"
+      shift
+      shift
+      ;;
+    --kubelet-ca-cert)
+      export KUBELET_CA_CERT="$2"
+      shift
+      shift
+      ;;
+    *) # Ignore unsupported args
+      shift
+      ;;
+  esac
+done
+
+echo "KUBELET_EXTRA_ARGS=$KUBELET_EXTRA_ARGS"
+echo "CLUSTER_DNS=$CLUSTER_DNS"
+echo "APISERVER_ENDPOINT=$APISERVER_ENDPOINT"
+
+if [[ -n $KUBELET_EXTRA_ARGS ]]; then
+  OKE_EXTRA_ARGS="--kubelet-extra-args=\"$KUBELET_EXTRA_ARGS\""
+fi
+
+if [[ -e "/usr/bin/nvidia-smi" ]]; then
+  OKE_EXTRA_ARGS="--manage-gpu-services=true $OKE_EXTRA_ARGS"
+fi
+
+echo "OKE_EXTRA_ARGS=$OKE_EXTRA_ARGS"
+
+if [[ -n "$APISERVER_ENDPOINT" && -n "$KUBELET_CA_CERT" ]]; then
+  mkdir -p /etc/kubernetes
+  mkdir -p /etc/oke
+  echo OKE_EXTRA_ARGS=${OKE_EXTRA_ARGS} > /etc/oke/oke.conf
+  echo CLUSTER_DNS=${CLUSTER_DNS} >> /etc/oke/oke.conf
+  echo APISERVER_ENDPOINT=${APISERVER_ENDPOINT} >> /etc/oke/oke.conf
+  echo "$KUBELET_CA_CERT" | base64 -d > /etc/kubernetes/ca.crt
+elif has_api_server; then
+  mkdir -p /etc/kubernetes
+  mkdir -p /etc/oke
+  OKE_EXTRA_ARGS="--skip-growfs=true $OKE_EXTRA_ARGS"
+  echo OKE_EXTRA_ARGS=${OKE_EXTRA_ARGS} > /etc/oke/oke.conf
+  echo CLUSTER_DNS=${CLUSTER_DNS} >> /etc/oke/oke.conf
+else
+  echo "--apiserver-endpoint and/or --kubelet-ca-cert args must be set"
+  exit 1
+fi
+mkdir -p /etc/systemd/system/oke.service.d
+echo -e "[Service]\nEnvironmentFile=/etc/oke/oke.conf" > /etc/systemd/system/oke.service.d/10-args.conf
+echo -e "StandardOutput=journal+console" >> /etc/systemd/system/oke.service.d/10-args.conf
+echo -e "StandardError=journal+console" >> /etc/systemd/system/oke.service.d/10-args.conf
+mkdir -p /etc/systemd/system/kubelet.service.d
+echo -e "[Service]\nStandardError=journal+console" > /etc/systemd/system/kubelet.service.d/10-args.conf
+swapoff -a || true
+systemctl enable --now oke
+restart_crio || true
+touch /etc/.oke_init_complete || true
+echo "$(date) Finished OKE Provisioning"
